820d72957783708a0af3b0717e267508c651578f
@abhishek.mohan <abhishek.mohan@surboard.se>
2023-03-21 16:14:46 +0530

    called a json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { map, mapTo, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,26 @@ export class AppComponent {
   title = 'observables';
   arr:number[]=[];
   res!:number;
+  content:any
+  constructor(private http: HttpClient){}
 
   observe(){
-    console.log("observe started")
+    console.log("observe started");
 
     of(1,2,3,4)
       .pipe(mapTo("Success"))
       .subscribe(console.log)
+
+    // this.http.get("https://jsonplaceholder.typicode.com/todos").subscribe((v:any)=>console.log(v)) //printing the data
+
+    this.content = this.http.get("https://jsonplaceholder.typicode.com/todos").pipe(
+      map((res:any) =>
+        res.map((data:any) => {
+          return{
+            title:data.title,
+            completed:data.completed
+          }
+    })))
   }
 
 
